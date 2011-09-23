1b01d2c8f4064846f794a21cfd56a4b9bfcd1cb4
mashannon168 <mashannon168>
2011-09-23 15:23:37 +0000

    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@80112 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- jive/02_jive_data_setup.sql
+++ jive/02_jive_data_setup.sql
@@ -1,3 +1,4 @@
+database jive;
 
 INSERT INTO 'informix'.template_project_forum (template_project_forum_id, template_id, name, description, display_order, direct_project_type_id) 
        VALUES (1, NULL, 'Development Strategies', 'Discuss Project Development Strategies here', 1, NULL);
