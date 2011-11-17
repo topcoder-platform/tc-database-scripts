8cbf473585c4ae4f0f888e4c2b41ae833d4e87f9
mashannon168 <mashannon168>
2011-11-17 18:29:16 +0000

    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@80404 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- informixoltp/02_informixoltp_data_setup.sql
+++ informixoltp/02_informixoltp_data_setup.sql
@@ -11853,3 +11853,14 @@ INSERT INTO 'informix'.path(path_id, path, modify_date) VALUES(24,'/i/m/preview/
 INSERT INTO 'informix'.path(path_id, path, modify_date) VALUES(25,'/i/m/removed/',current);
 
 
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (1,'Copilot','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (2,'Spec Review','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (3,'Winner','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (4,'Review','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (5,'Reliability','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (6,'Bugs','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (7,'Build','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (8,'Misc','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (9,'Contest Fee','132456',current,'132456',current);
+insert into invoice_type_lu (invoice_type_id, name, create_user, create_date, modify_user, modify_date) values (10,'Digital Run','132456',current,'132456',current);
+
