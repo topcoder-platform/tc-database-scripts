ee766d9afdec2346dcf78af4b0756b6ee65522d4
mashannon168 <mashannon168>
2011-12-06 17:13:34 +0000

    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@80503 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- informixoltp/02_informixoltp_data_setup.sql
+++ informixoltp/02_informixoltp_data_setup.sql
@@ -1205,8 +1205,8 @@ INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES 
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (28971, 'amd_mm3_registrants', 1);
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (28989, 'News and Events - NorthEastUSA', 1);
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (30859, 'member_vcard', 4);
-
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (31009, 'member_image', 4);
+INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (31321, 'coder_image_data_list', 11005);
 
 INSERT INTO 'informix'.input_lu(input_id,input_code,data_type_id,input_desc) VALUES (5, 'sda', 1003, 'Start date');
 INSERT INTO 'informix'.input_lu(input_id,input_code,data_type_id,input_desc) VALUES (6, 'ed', 1003, 'End date');
@@ -3420,6 +3420,7 @@ INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) value
 INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (30086, null, 'spec_review_stats', 0, null);
 INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (31196, null, 'member_vcard', 0, null);
 INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) VALUES (31356, null, 'coder_image_data', 0, 0);
+INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) VALUES (31690, null, 'coder_image_data_list', 0, 0);
 
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (29539, 29836, 0);
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (22, 50, 1);
@@ -4614,8 +4615,8 @@ INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (28989, 29246, 0);
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (12211, 30086, 11);
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (30859, 31196, NULL);
-
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (31009, 31356, 1);
+INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (31321, 31690, 1);
 
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (29836, 'N', NULL, 25390, 0);
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (50, 'N', NULL, 18, 1);
@@ -4935,8 +4936,8 @@ INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (29106, NULL, NULL, 25260, NULL);
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (30086, 'N', NULL, 3, 0);
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (31196, NULL, NULL, 1000, NULL);
-
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (31356, NULL, NULL, 3, NULL);
+INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (31690, NULL, NULL, 25031, NULL);
 
 INSERT INTO 'informix'.assignment_document_template(assignment_document_template_id,assignment_document_type_id,assignment_document_template_text,cur_version,assignment_document_template_name) VALUES (2000, 1, null, 0, null);
 INSERT INTO 'informix'.assignment_document_template(assignment_document_template_id,assignment_document_type_id,assignment_document_template_text,cur_version,assignment_document_template_name) VALUES (2001, 2, null, 0, null);
