bfb7d50189ac5f84426fba46bfee0b33f0e6562c
VolodymyrK <VolodymyrK>
2012-02-21 07:26:42 +0000

    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@80898 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- informixoltp/01_informixoltp_main_schema.sql
+++ informixoltp/01_informixoltp_main_schema.sql
@@ -1596,7 +1596,6 @@ create table 'informix'.payment_detail (
     total_amount DECIMAL(12,2),
     installment_number DECIMAL(3,0) default 1,
     digital_run_track_id DECIMAL(10,0),
-    invoice_number VARCHAR(11),
     jira_issue_id VARCHAR(100),
     create_user DECIMAL(10,0)
 )
