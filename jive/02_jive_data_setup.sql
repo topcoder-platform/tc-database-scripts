0a9a5d71677f0824268d542f5a12855d1aba3f2f
mashannon168 <mashannon168>
2012-05-25 14:11:43 +0000

    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@81381 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- jive/02_jive_data_setup.sql
+++ jive/02_jive_data_setup.sql
@@ -1,13 +1,9 @@
 database jive;
 
-INSERT INTO 'informix'.template_project_forum (template_project_forum_id, template_id, name, description, display_order, direct_project_type_id) 
-       VALUES (1, NULL, 'Development Strategies', 'Discuss Project Development Strategies here', 1, NULL);
+
 INSERT INTO 'informix'.template_project_forum (template_project_forum_id, template_id, name, description, display_order, direct_project_type_id) 
        VALUES (2, NULL, 'Project Requirements', 'Discuss Project Requirements here', 1, NULL);
-INSERT INTO 'informix'.template_project_forum (template_project_forum_id, template_id, name, description, display_order, direct_project_type_id) 
-       VALUES (3, NULL, 'Project Issues and Bugs', 'Discuss issues and bugs of the project here', 1, NULL);
-INSERT INTO 'informix'.template_project_forum (template_project_forum_id, template_id, name, description, display_order, direct_project_type_id) 
-       VALUES (4, NULL, 'TopCoder Platform Support', 'Need a help on TopCoder Platforms for your project', 1, NULL);
+
 
 INSERT INTO JIVEUSERPERM VALUES(17,-1,1,1,59)
 ;
@@ -375,4 +371,6 @@ insert into jivecategoryprop values(21,'showEmptyForumsOnMain', 'true');
 insert into jivecategoryprop values (21,'leftNavName', 'High School Matches');
 insert into jivecategoryprop values(22,'showEmptyForumsOnMain', 'true');
 insert into jivecategoryprop values (22,'leftNavName', 'Software Forums');
+
+insert into jivegroup values (6, 'Software Admins', 'Software Admins', 1170993541468, 1332771806201);
 
