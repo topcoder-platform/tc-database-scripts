1f7f2da558addb6753e65d16273b05efae72766e
mashannon168 <mashannon168>
2012-09-13 14:30:05 +0000

    copilot feedback.
    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@81930 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- tcs_catalog/02_tcs_catalog_data_setup.sql
+++ tcs_catalog/02_tcs_catalog_data_setup.sql
@@ -783,6 +783,7 @@ INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES 
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (31830, 'get_projects_from_billing_and_invoice_number', 13337);
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (31831, 'enterprise_dashboard_contests_pipeline', 13337);
 INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (31849, 'my_active_projects_contest_ids', 13337);
+INSERT INTO 'informix'.command(command_id,command_desc,command_group_id) VALUES (31859, 'get_all_copilot_feedback', 13337);
 
 INSERT INTO 'informix'.input_lu(input_id,input_code,data_type_id,input_desc) VALUES (13341, 'ph', 1001, 'phase id');
 INSERT INTO 'informix'.input_lu(input_id,input_code,data_type_id,input_desc) VALUES (13342, 'pj', 1001, 'project id');
@@ -1881,6 +1882,9 @@ INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) value
 INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (32257, null, 'get_projects_from_billing_and_invoice_number', 0, null);
 INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (32258, null, 'enterprise_dashboard_contests_pipeline', 0, null);
 INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (32276, null, 'my_active_projects_contest_ids', 0, null);
+INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (32286, null, 'get_all_copilot_feedback', 0, null);
+INSERT INTO 'informix'.query (query_id, text, name, ranking, column_index) values (32287, null, 'copilot_feedback', 0, null);
+
 
 -- query parameter for 13717 (review_projects) added (pt) per TCS 2.2.0
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (13717, 'N', NULL, 25190, 1);
@@ -2318,6 +2322,7 @@ INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (32258, NULL, NULL, 25600, 0);
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (32258, NULL, NULL, 25601, 0);
 INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (32276, NULL, NULL, 13347, 0);
+INSERT INTO 'informix'.query_input_xref(query_id,optional,default_value,input_id,sort_order) VALUES (32287, NULL, NULL, 13347, 0);
 
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (11010, 11010, NULL);
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (12153, 12154, 0);
@@ -2734,6 +2739,8 @@ INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (31830, 32257, 0);
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (31831, 32258, 0);
 INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (31849, 32276, 0);
+INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (31859, 32286, 0);
+INSERT INTO 'informix'.command_query_xref(command_id,query_id,sort_order) VALUES (30389, 32287, 0);
 
 INSERT INTO 'informix'.user_master(login_id, last_login_time, num_logins, status_id) VALUES (132456, current, 0, 1);
 INSERT INTO 'informix'.user_master(login_id, last_login_time, num_logins, status_id) VALUES (20, current, 0, 1);
@@ -2975,4 +2982,16 @@ INSERT INTO 'informix'.evaluation_type_lu (evaluation_type_id,name,description,c
     (5, 'Combined', 'Combined - 0 points', 'System', current, 'System', current);
 INSERT INTO 'informix'.evaluation_type_lu (evaluation_type_id,name,description,create_user,create_date,modify_user,modify_date) VALUES
     (6, 'Incorrect', 'Incorrect - 10 (negative) points', 'System', current, 'System', current);
+	
+	
+INSERT INTO copilot_project_info_type (copilot_project_info_type_id, name,create_user, create_date, modify_user, modify_date ) 
+            VALUES(1, 'feedback_text', 'system', CURRENT, 'system', CURRENT);
+INSERT INTO copilot_project_info_type (copilot_project_info_type_id, name,create_user, create_date, modify_user, modify_date ) 
+            VALUES(2, 'feedback_answer', 'system', CURRENT, 'system', CURRENT);
+INSERT INTO copilot_project_info_type (copilot_project_info_type_id, name,create_user, create_date, modify_user, modify_date ) 
+            VALUES(3, 'feedback_author', 'system', CURRENT, 'system', CURRENT);
+INSERT INTO copilot_project_info_type (copilot_project_info_type_id, name,create_user, create_date, modify_user, modify_date ) 
+            VALUES(4, 'feedback_status', 'system', CURRENT, 'system', CURRENT);
+
+
 
