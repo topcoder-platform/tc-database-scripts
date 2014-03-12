fc9098568d8d85d97d60facf56037acc1541d4a6
mashannon168 <mashannon168>
2014-03-12 16:33:35 +0000

    new procedure platform_list()
    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@84601 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- tcs_catalog/04_tcs_catalog_synonyms_procedures_and_triggers.sql
+++ tcs_catalog/04_tcs_catalog_synonyms_procedures_and_triggers.sql
@@ -523,6 +523,38 @@ end procedure;
 
 grant execute on technology_list to public as informix;
 
+create procedure platform_list( i_project_id decimal(12) )
+    returning lvarchar(1000);
+
+    define o_cat_list lvarchar(1000);
+    define t_cat_desc lvarchar(1000);
+
+    let o_cat_list = "";
+
+    foreach
+
+        select ppl.name
+            into t_cat_desc
+        from project_platform_lu ppl
+        inner join project_platform pp
+        on ppl.project_platform_id = pp.project_platform_id
+        where pp.project_id = i_project_id
+        order by ppl.name
+
+        if length(o_cat_list) = 0 then
+            let o_cat_list = t_cat_desc;
+        else
+            let o_cat_list = o_cat_list || ", " || t_cat_desc;
+        end if
+
+    end foreach
+
+return o_cat_list;
+
+end procedure;
+
+grant execute on platform_list to public as informix;
+
 create procedure proc_resource_insert( r_id INT, resoure_info_type_id INT, value VARCHAR(255))
    
     if (resoure_info_type_id == 1 and exists (select resource_id from resource where resource_id = r_id and resource_role_id = 1)) then
