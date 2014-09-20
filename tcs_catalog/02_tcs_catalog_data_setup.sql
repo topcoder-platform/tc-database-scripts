f0b54e03765b8b09f286905d0a147f0369eb8b42
hohosky <hohosky>
2014-09-20 12:29:17 +0000

    Add Data Science technology type
    git-svn-id: https://coder.topcoder.com/internal/database/scripts/trunk@85176 cb5b18d2-80dd-4a81-9b1c-945e1ee644f9

--- tcs_catalog/02_tcs_catalog_data_setup.sql
+++ tcs_catalog/02_tcs_catalog_data_setup.sql
@@ -456,6 +456,7 @@ INSERT INTO 'informix'.technology_types(technology_type_id,technology_name,descr
 INSERT INTO 'informix'.technology_types(technology_type_id,technology_name,description,status_id) VALUES (27203172, 'PHP', 'PHP', 1);
 INSERT INTO 'informix'.technology_types(technology_type_id,technology_name,description,status_id) VALUES (27203173, 'Ruby', 'Ruby', 1);
 INSERT INTO 'informix'.technology_types(technology_type_id,technology_name,description,status_id) VALUES (27203174, 'Objective C', 'Objective C', 1);
+INSERT INTO 'informix'.technology_types(technology_type_id,technology_name,description,status_id) VALUES (27203175, 'Data Science', 'Data Science', 1);
 INSERT INTO 'informix'.command_group_lu(command_group_id,command_group_name) VALUES (11004, 'Util');
 INSERT INTO 'informix'.command_group_lu(command_group_id,command_group_name) VALUES (13337, 'development');
 INSERT INTO 'informix'.command_group_lu(command_group_id,command_group_name) VALUES (13343, 'report');
