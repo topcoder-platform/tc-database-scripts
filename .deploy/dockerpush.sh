ca7886d8d5d187f6c2f9c32206b497dbe31641b6
Guiqiang Zhang <skyhit@gmail.com>
2017-03-23 09:57:37 +0800

    update circleci

---
+++ .deploy/dockerpush.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Building docker image"
+echo "############################"
+docker build -t $DOCKER_REPO:$CIRCLE_SHA1 .
+
+echo "Logging into docker"
+echo "############################"
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
+
+echo "Pushing docker image"
+echo "############################"
+docker push $DOCKER_REPO:$CIRCLE_SHA1
+docker tag $DOCKER_REPO:$CIRCLE_SHA1 $DOCKER_REPO:latest
+docker push $DOCKER_REPO:latest
