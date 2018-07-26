6a8f0f6171a8d55bfad75c699783b521f851cd55
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2018-07-26 13:14:16 +0530

    Update dockerpush.sh

--- .deploy/dockerpush.sh
+++ .deploy/dockerpush.sh
@@ -7,10 +7,11 @@ docker build -t $DOCKER_REPO:$CIRCLE_SHA1 .
 
 echo "Logging into docker"
 echo "############################"
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
+#docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWD
 
 echo "Pushing docker image"
 echo "############################"
-docker push $DOCKER_REPO:$CIRCLE_SHA1
-docker tag $DOCKER_REPO:$CIRCLE_SHA1 $DOCKER_REPO:latest
-docker push $DOCKER_REPO:latest
+#docker push $DOCKER_REPO:$CIRCLE_SHA1
+#docker tag $DOCKER_REPO:$CIRCLE_SHA1 $DOCKER_REPO:latest
+#docker push $DOCKER_REPO:latest
