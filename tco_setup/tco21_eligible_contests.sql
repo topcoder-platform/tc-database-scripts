f0679837e53b221da4fd4a3c8360979340414e9a
m-yadav <manish.yadav@topcoder.com>
2020-09-11 07:11:14 +0530

    excluding challenge 30137832 as per ticket 68709

--- tco_setup/tco21_eligible_contests.sql
+++ tco_setup/tco21_eligible_contests.sql
@@ -425,6 +425,7 @@ WHERE p.project_status_id = 1
 -- and mod(p.project_id, 2) = 0
    and p.project_id not in (select ce.contest_id from contest_eligibility ce) --excluding private challenges
    and p.project_id not in ( 30138384 ) --excluding as per support ticket 67942
+   and p.project_id not in ( 30137832 ) --excluding as per support Ticket 68709
    and 1 = 1; 
 
 -- Stage 2
